a50b5cba2f668aad772df019b38b35345689173d
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-01-24 16:13:39 -0600

    Forked and cloned down client week project to change files to run on TravisCI

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ module.exports = {
   entry: "./js/index.js",
   devtool: "source-map",
   output: {
-      path: "./dist",
+      path: "public/dist",
       filename: "bundle.js"
   },
-module: {
+  module: {
       loaders: [
         {
           test: /\.js$/,
